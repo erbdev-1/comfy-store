7e064ced97c53a8e9b06314b6b9e398ab51d57c9
erbdev <erbdev@hotmail.com>
2024-09-01 00:58:27 +0100

    first commit

---
+++ src/utils/index.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const productionUrl = "https://strapi-store-server.onrender.com/api";
+
+export const customFetch = axios.create({
+  baseURL: productionUrl,
+});
+
+export const formatPrice = (price) => {
+  const poundsAmount = new Intl.NumberFormat("en-GB", {
+    style: "currency",
+    currency: "GBP",
+  }).format((price / 100).toFixed(2));
+  return poundsAmount;
+};
+export const generateAmountOptions = (number) => {
+  return Array.from({ length: number }, (_, index) => {
+    const amount = index + 1;
+
+    return (
+      <option key={amount} value={amount}>
+        {amount}
+      </option>
+    );
+  });
+};
